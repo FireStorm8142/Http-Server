447249ae723b5fed54b9cb72f7f19b5329db2208
Johan Paul <johanjohn405@gmail.com>
2026-03-02 00:33:37 +0530

    Created json.java and made changes to configmanager

--- httpserver/src/main/java/com/johan/httpserver/config/configmanager.java
+++ httpserver/src/main/java/com/johan/httpserver/config/configmanager.java
@@ -2,8 +2,25 @@ package com.johan.httpserver.config;
 
 public class configmanager {
     private static configmanager myconfigmanager;
+    private static configuration mycurrentconfig;
     
     private configmanager(){
         
+    }
+
+    public static configmanager getInstance(){
+        if(myconfigmanager==null)
+            myconfigmanager= new configmanager();
+        return myconfigmanager;
+    }
+
+    //method for loading a config file by the path provided
+    public void loadConfigFile(String filePath){
+
+    }
+
+    //method for returning currently loaded config
+    public void getCurrentConfig(){
+
     }
 }
