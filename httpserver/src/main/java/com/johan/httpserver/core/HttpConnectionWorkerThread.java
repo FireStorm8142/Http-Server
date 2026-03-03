5eab5986afbae9a176c8ff70df22d813c68b0f02
Johan Paul <johanjohn405@gmail.com>
2026-03-03 21:12:57 +0530

    Added MultiThreading to handle multiples connections

---
+++ httpserver/src/main/java/com/johan/httpserver/core/HttpConnectionWorkerThread.java
@@ -0,0 +1,61 @@
+package com.johan.httpserver.core;
+
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class HttpConnectionWorkerThread extends Thread{
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(HttpConnectionWorkerThread.class);
+    private Socket socket;
+    public HttpConnectionWorkerThread(Socket socket){
+        this.socket=socket;
+    }
+
+    @Override
+    public void run(){
+        InputStream ipStream = null;
+        OutputStream opStream = null;
+        try {
+            ipStream = socket.getInputStream();
+            opStream = socket.getOutputStream();
+
+            String html = "<html><head><title>Http Server</title></head><body><h1>This page was served using my Java http server</h1></body></html>";
+            final String CRLF = "\r\n"; //13, 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + // Status Line : HTTP Version Response_code Response_msg
+                            "Content-Length: " + html.getBytes().length + CRLF + // Header
+                            CRLF +
+                            html +
+                            CRLF + CRLF;
+
+            opStream.write(response.getBytes());
+
+            LOGGER.info("Connection Processing Finished");
+        }catch(IOException e){
+            LOGGER.error("Problem with communication", e);
+        }
+        finally {
+            if(ipStream!=null){
+                try {
+                    ipStream.close();
+                }catch (IOException e) {}
+            }
+            if(opStream!=null){
+                try{
+                    opStream.close();
+                }catch (IOException e){}
+            }
+            if(socket!=null){
+                try{
+                    socket.close();
+                }catch (IOException e){}
+            }
+        }
+    }
+}
--- httpserver/src/main/java/com/johan/httpserver/httpserver.java
+++ httpserver/src/main/java/com/johan/httpserver/httpserver.java
@@ -2,16 +2,31 @@ package com.johan.httpserver;
 
 import com.johan.httpserver.config.configuration;
 import com.johan.httpserver.config.configmanager;
+import com.johan.httpserver.core.ServerListenerThread;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import java.io.IOException;
 
 public class httpserver {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(httpserver.class);
     public static void main(String[] args)
     {
+        LOGGER.info("Server Starting");
+
         System.out.println("Server starting...");
 
         configmanager.getInstance().loadConfigFile(("httpserver/src/main/resources/http.json"));
         configuration conf = configmanager.getInstance().getCurrentConfig();
 
-        System.out.println("Using port: "+conf.getPort());
-        System.out.println("Using WebRoot: "+conf.getWebroot());
+        LOGGER.info("Using Port: "+conf.getPort());
+        LOGGER.info("Using WebRoot: "+ conf.getWebroot());
+
+        try {
+            ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
